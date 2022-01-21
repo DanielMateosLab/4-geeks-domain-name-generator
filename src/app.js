cf4de175d3ca5882c13467fe0b3fad5953db1814
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-01-21 20:53:01 +0000

    Add first version of the DNG

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+/**
+ * TODO:
+ * - display domains as little cards with random text-color for each word.
+ * - Copy domain to clipboard on-click
+ * - Use css transition to increase size of hovered domain
+ */
+
+let pronombres = ["un", "vuestro", "su"];
+let adjetivos = ["increible", "absurdo", "descabellado"];
+let nombres = ["pastel", "delito", "plan"];
+let dominios = [".com", ".es", ".io"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (const pronombre of pronombres) {
+    for (const adjetivo of adjetivos) {
+      for (const nombre of nombres) {
+        for (const dominio of dominios) {
+          const pronombreHTML = wrapInColoredSpan(pronombre, "success");
+          const adjetivoHTML = wrapInColoredSpan(adjetivo, "dark");
+          const nombreHTML = wrapInColoredSpan(nombre, "danger");
+          const dominioHTML = wrapInColoredSpan(dominio, "info");
+
+          document.querySelector(
+            "#domains"
+          ).innerHTML += `<div>${pronombreHTML +
+            adjetivoHTML +
+            nombreHTML +
+            dominioHTML}</div>`;
+        }
+      }
+    }
+  }
 };
+
+/** @argument color - one of the bootstrap variants (sucess, secondary...) */
+function wrapInColoredSpan(text, color) {
+  return `<span class='text-${color}'>${text}</span>`;
+}
