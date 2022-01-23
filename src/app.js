6c9af07dcd3c4668776ac14b24354347b2c46272
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-01-23 16:23:48 +0000

    Refactor, document and clean code

--- src/app.js
+++ src/app.js
@@ -10,21 +10,50 @@ let dominiosComerciales = [".com", ".es", ".io"];
 let colors = ["success", "dark", "danger", "info"];
 
 window.onload = function() {
+  let domainElements = generateDomainButtonElements(
+    pronombres,
+    adjetivos,
+    nombres,
+    dominiosComerciales,
+    colors
+  );
+  const domainsContainer = document.querySelector("#domains");
+
+  domainElements = shuffleArray(domainElements);
+  domainElements.forEach(domainName => domainsContainer.append(domainName));
+};
+
+/** Returns an array of HTML buttons with the given domain components,
+ *  each component having a different random color of given colors.
+ */
+function generateDomainButtonElements(
+  pronombres,
+  adjetivos,
+  nombres,
+  dominiosComerciales,
+  colors
+) {
+  let domainElements = [];
+
   for (const pronombre of pronombres) {
     for (const adjetivo of adjetivos) {
       for (const nombre of nombres) {
         for (const domComercial of dominiosComerciales) {
           let domainComponents = [pronombre, adjetivo, nombre, domComercial];
+          // Here we get an array with 4 randomly ordered colors.
           const shuffledColors = shuffleArray(colors);
 
+          // Convert each domain component string in a span HTMLElement with a
+          // random color
           domainComponents = domainComponents.map(component =>
             wrapInColoredSpan(component, shuffledColors.pop())
           );
 
+          // Join all the domain components in a button element with custom styles and
+          // onClick functionality
           const domainButton = document.createElement("button");
           domainButton.className = "domain";
           domainButton.innerHTML = domainComponents.join("");
-
           domainButton.addEventListener("click", function() {
             const domainText = domainButton.innerText;
 
@@ -32,18 +61,22 @@ window.onload = function() {
             navigator.clipboard.writeText(domainText);
           });
 
-          document.querySelector("#domains").append(domainButton);
+          domainElements.push(domainButton);
         }
       }
     }
   }
-};
+
+  return domainElements;
+}
 
 /** @argument color - one of the bootstrap variants (sucess, secondary...) */
 function wrapInColoredSpan(text, color) {
   return `<span class='text-${color}'>${text}</span>`;
 }
 
+/** Generates an alert informing the user that the domainName has
+ *  been copied to clipboard */
 function alert(domainName) {
   const wrapper = document.createElement("div");
   wrapper.innerHTML = `
@@ -58,6 +91,8 @@ function alert(domainName) {
     </div>
     `;
 
+  // Replacing the innerHTML of the place we avoid multiple alerts being present
+  // which is good because only the last alert content is valid
   document.querySelector(".alert-placeholder").innerHTML = wrapper.innerHTML;
 }
 
@@ -74,6 +109,7 @@ function shuffleArray(array) {
   return shuffledArray;
 }
 
+/** Returns a random number between 0 and limit (limit not included) */
 function getRandomNumber(limit) {
   return Math.floor(Math.random() * limit);
 }
